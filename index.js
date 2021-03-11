5290a83164cd8042366d45b27a6c8989a9960cfd
iemong <kiyoshidainagon@gmail.com>
2021-03-11 23:56:46 +0900

    音の再生までできるようにする

---
+++ index.js
@@ -0,0 +1,14 @@
+const player = require('node-wav-player');
+
+const sound1 = './sounds/power1.wav'
+const sound2 = './sounds/power2.wav'
+const sound3 = './sounds/power3.wav'
+
+player.play({
+    path: sound1,
+    sync: true,
+}).then(() => {
+    console.log('The wav file started to be played successfully.');
+}).catch((error) => {
+    console.error(error);
+});
