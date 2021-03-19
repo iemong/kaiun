b573d87a3272af486573f530b2b7d599b1d8b70f
iemong <kiyoshidainagon@gmail.com>
2021-03-19 19:20:05 +0900

    gpio触れるようにする

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const player = require('node-wav-player');
+const gpio = require('rpi-gpio').promise;
 
 const sound1 = './sounds/power1.wav'
 const sound2 = './sounds/power2.wav'
 const sound3 = './sounds/power3.wav'
 
-player.play({
-    path: sound1,
-    sync: true,
-}).then(() => {
-    console.log('The wav file started to be played successfully.');
-}).catch((error) => {
-    console.error(error);
-});
+const playSound = () => {
+    player.play({
+        path: sound1,
+        sync: true,
+    }).then(() => {
+        console.log('The wav file started to be played successfully.');
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+const setup = () => {
+    gpio.setup(13, gpio.DIR_OUT).then(() => gpio.write(7, true)).catch(e => console.error(e))
+}
+
+setup()
